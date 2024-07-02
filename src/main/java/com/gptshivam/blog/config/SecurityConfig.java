f784f360a10225eede67b2ef8b1fbd5e6587826d
Shivam Kumar gupta <gptshivam595@gmail.com>
2024-07-02 23:27:12 +0530

    working on jwt

--- src/main/java/com/gptshivam/blog/config/SecurityConfig.java
+++ src/main/java/com/gptshivam/blog/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+	
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
